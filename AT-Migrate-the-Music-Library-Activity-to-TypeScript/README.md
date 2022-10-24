7ad002b40db29dbc293aa08678133a3a0bf44bda
Joram Obbus <97580581+mandatedlime@users.noreply.github.com>
2022-10-23 18:16:49 -0700

    Done

---
+++ AT-Migrate-the-Music-Library-Activity-to-TypeScript/README.md
@@ -0,0 +1,4 @@
+# 11.2.7-code-along-solution
+
+TypeScript migration of https://github.com/HackerUSA-CE/music_library_solution
+# Music-Library-
---
+++ AT-Migrate-the-Music-Library-Activity-to-TypeScript/src/react-app-env.d.ts
@@ -0,0 +1,9 @@
+/// <reference types="react-scripts" />
+
+interface ItemProps {
+    trackName: string
+    collectionName: string
+    primaryGenreName: string
+    releaseDate: string
+    artworkUrl100: string
+}
